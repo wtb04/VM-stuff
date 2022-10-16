d26c83e51dc02b9a363c6d76fa54f8c0763b0b01
wtb04 <aagknaag@hotmail.com>
2022-10-16 12:00:09 +0200

    Update Install.sh

--- Install.sh
+++ Install.sh
@@ -1,14 +1,14 @@
 # Install fortume mod and cowsay
 sudo apt-get update
-sudo apt install cowsay fortune-mod -Y
+sudo apt install cowsay fortune-mod -y
 
 #Install speedtest cli
-sudo apt-get install curl -Y
+sudo apt-get install curl -y
 curl -s https://packagecloud.io/install/repositories/ookla/speedtest-cli/script.deb.sh | sudo bash
 sudo apt-get install speedtest
 
 #Update alles
-sudo apt-get upgrade
+sudo apt-get upgrade -y
 
 #Bash_profile aanzetten
 curl -o .bash_profile https://raw.githubusercontent.com/wtb04/VM-stuff/main/.bash_profile
